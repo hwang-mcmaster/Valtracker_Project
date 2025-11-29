c29221c43f07a45d1f2a651a8a024496cca61b17
Haobo Wang <wangh518@mcmaster.ca>
2025-11-29 16:05:42 -0500

    new changes to tune the results

--- app/services/stats_service.py
+++ app/services/stats_service.py
@@ -1,4 +1,5 @@
 from app.adapters.esports_adapter import team_matches
 
+
 def recent_for_team(team_name: str, n: int = 5):
-    return team_matches(team_name, n)
+    return team_matches(team_name, completed_limit=n, upcoming_limit=n)
